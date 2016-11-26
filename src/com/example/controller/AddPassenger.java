1c305e9b2bc7187ed0870668e3f96c87e2fe1c95
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-26 11:31:46 +0100

    Add errors check for "firstName" and DOB
    added code so that attribute "error" is set, when firstName is empty or when DOB is in the wrong format
    
    Also user is redirected if

--- src/com/example/controller/AddPassenger.java
+++ src/com/example/controller/AddPassenger.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Servlet implementation class AddPassenger
@@ -44,27 +46,44 @@ public class AddPassenger extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        doGet(request, response);
+        // initially set "error" to false, to avoid NullPointer
+        request.setAttribute("error", false);
         String firstName = request.getParameter("first-name");
 
         if (firstName.length() == 0) {
             request.setAttribute("error", true);
-            response.sendRedirect("/");
+            request.setAttribute("first_name error", true);
         }
-
 
         String lastName = request.getParameter("last-name");
         String dateOfBirthRaw = request.getParameter("date-of-birth");
-        String[] dateOfBirthArray = dateOfBirthRaw.split("\\/");
+        Pattern dateOfBirthPattern = Pattern.compile("^\\d\\d/\\d\\d/\\d\\d\\d\\d$");
+        Matcher matcher = dateOfBirthPattern.matcher(dateOfBirthRaw);
+        if (matcher.matches()) {
+            String[] dateOfBirthArray = dateOfBirthRaw.split("\\/");
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(
-                Integer.parseInt(dateOfBirthArray[0]),
-                Integer.parseInt(dateOfBirthArray[1]),
-                Integer.parseInt(dateOfBirthArray[2])
-        );
-        Date dateOfBirth = calendar.getTime();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(
+                    Integer.parseInt(dateOfBirthArray[0]),
+                    Integer.parseInt(dateOfBirthArray[1]),
+                    Integer.parseInt(dateOfBirthArray[2])
+            );
+            Date dateOfBirth = calendar.getTime();
+        } else {
+            request.setAttribute("error date-of-birth", true);
+        }
 
         String rawGender = request.getParameter("gender");
+
+        // if there are errors, we redirect:
+        if ((Boolean) request.getAttribute("error")) {
+            // This getting request dispatcher and view.forward
+            // works like a redirect.
+            RequestDispatcher view = request.getRequestDispatcher(
+                    "WEB-INF/views/add-passenger.jsp"
+            );
+            view.forward(request, response);
+        }
 
         System.out.println();
     }
