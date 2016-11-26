1efa8d246b6402d089841be916644fcafa46ae01
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-26 18:22:13 +0100

    Refactor doPost in AddPassengerServlet
    made attributes set consistent.
    
    added "error" attribute for lastName.
    
    provided comments everywhere possible.
    
    Made regex cleaner

--- src/com/example/controller/AddPassengerServlet.java
+++ src/com/example/controller/AddPassengerServlet.java
@@ -54,21 +54,29 @@ public class AddPassengerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
-        // initially set "error" to false, to avoid NullPointer
-        request.setAttribute("error", false);
-        String firstName = request.getParameter("first-name");
+        // initially set "errors" to false, to avoid NullPointerException
+        request.setAttribute("errors", false);
 
+        // check if first name is right
+        String firstName = request.getParameter("first-name");
         if (firstName.length() == 0) {
             request.setAttribute("error", true);
-            request.setAttribute("first_name error", true);
+            request.setAttribute("first-name error", true);
         }
 
+        // check if last name is right
         String lastName = request.getParameter("last-name");
+        if (lastName.length() == 0) {
+            request.setAttribute("error", true);
+            request.setAttribute("last-name error", true);
+        }
+
+        // check if date of birth is right
         String dateOfBirthRaw = request.getParameter("date-of-birth");
-        Pattern dateOfBirthPattern = Pattern.compile("^\\d\\d/\\d\\d/\\d\\d\\d\\d$");
+        Pattern dateOfBirthPattern = Pattern.compile("^\\d{2}/\\d{2}/\\d{4}$");
         Matcher matcher = dateOfBirthPattern.matcher(dateOfBirthRaw);
         if (matcher.matches()) {
-            String[] dateOfBirthArray = dateOfBirthRaw.split("\\/");
+            String[] dateOfBirthArray = dateOfBirthRaw.split("/");
 
             Calendar calendar = Calendar.getInstance();
             calendar.set(
@@ -78,9 +86,10 @@ public class AddPassengerServlet extends HttpServlet {
             );
             Date dateOfBirth = calendar.getTime();
         } else {
-            request.setAttribute("error date-of-birth", true);
+            request.setAttribute("date-of-birth error", true);
         }
 
+        // gender is always right
         String rawGender = request.getParameter("gender");
 
         // if there are errors, we redirect:
