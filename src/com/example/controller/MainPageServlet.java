e576ff57c1be0df21eb6c0965f02e3f17a6ba7ad
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-26 13:44:43 +0100

    Add MainPageServlet class
    Very simple class extending HttpServlet, shows "index.jsp" on GET, and POST does nothing

---
+++ src/com/example/controller/MainPageServlet.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class MainPageServlet
+ */
+@WebServlet("/")
+public class MainPageServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public MainPageServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //response.getWriter().append("Served at: ").append(request.getContextPath());
+        RequestDispatcher view = request.getRequestDispatcher(
+                "WEB-INF/views/index.jsp"
+        );
+
+        view.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //doGet(request, response);
+        //initially set "error" to false, to avoid NullPointer
+    }
+
+}
