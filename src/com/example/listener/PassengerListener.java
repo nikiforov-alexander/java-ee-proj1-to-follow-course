b9ba3265c8a71338285b4cb75a8eda592528858c
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-28 16:21:31 +0100

    Add PassengerListener with constructor and empty overrides

---
+++ src/com/example/listener/PassengerListener.java
@@ -0,0 +1,23 @@
+package com.example.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class PassengerListener implements ServletContextListener{
+
+    public PassengerListener() {
+
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
