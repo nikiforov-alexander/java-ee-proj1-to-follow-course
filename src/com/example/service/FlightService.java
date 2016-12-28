3eeeea62cb7e1ea1cb25d8735ba93b71043c7d95
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-28 18:02:16 +0100

    Add FlightService second try
    Was error moving from GitKraken to Intellij

--- src/com/example/service/FlightService.java
+++ src/com/example/service/FlightService.java
@@ -1,4 +1,68 @@
 package com.example.service;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
 public class FlightService {
+
+    // properties
+
+    private Integer id = 234857;
+    private String from = "LA";
+    private String to = "London";
+    private Integer price = 400;
+
+    // getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    // constructors
+
+    public FlightService() {
+
+    }
+
+    // overrides
+
+    @Override
+    public String toString() {
+        return "FlightService{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
