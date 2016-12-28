9e9138bd63203a3ad9698bcb8b2915ac38a4e47e
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-28 16:26:41 +0100

    Fill contextInitialized method in PassengerListener
    Now it creates new "attribute" "passengers" in ServletContext

--- src/com/example/listener/PassengerListener.java
+++ src/com/example/listener/PassengerListener.java
@@ -1,8 +1,13 @@
 package com.example.listener;
 
+import com.example.model.Passenger;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebListener
 public class PassengerListener implements ServletContextListener{
@@ -11,9 +16,19 @@ public class PassengerListener implements ServletContextListener{
 
     }
 
+    /**
+     * creates "passengers" attribute as new {@code List<Passenger>}
+     * @param servletContextEvent that is used to extract
+     *                            {@code ServletContext}
+     */
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-
+        ServletContext servletContext =
+                servletContextEvent.getServletContext();
+        if (servletContext.getAttribute("passengers") == null){
+            List<Passenger> passengers = new ArrayList<>();
+            servletContext.setAttribute("passengers", passengers);
+        }
     }
 
     @Override
